76c19d40e77648db771d9b327e79bb26dc1383f2
Steve Behm <stevtbehm1@gmail.com>
2022-03-16 17:01:47 +0000

    completed code signal add algorithm.

---
+++ code-signal-add.js
@@ -0,0 +1,10 @@
+/*
+create a storage container for the output
+add the two inputs together and the result of that expression
+will be assigned to te output storage container
+display the output storage container
+*/
+function solution(param1, param2) {
+  var sum = param1 + param2;
+  return sum;
+}
